89a394579a38aa3c220751d75a741958af8293fe
Faraz Shaikh <farazzshaikh@gmail.com>
2021-05-07 03:18:43 +0530

    Added ability to add custom Fragment Shaders

--- build/three-csm.js
+++ build/three-csm.js
@@ -39,29 +39,42 @@ var THREE_CustomShaderMaterial = (function (exports, THREE) {
    */
   class CustomShaderMaterial extends THREE__namespace.ShaderMaterial {
     /**
+     *
+     * @typedef {Object} CustomShader
+     * @property {string} defines        Constant definitions like - "#define PI = 3.14;"
+     * @property {string} header         Code to be injected above main. Place function definitions here.
+     * @property {string} main           Code to be injected above main. Put main shader code here.
+     */
+
+    /**
      * Creates an instance of the <code>CustomShaderMaterial</code> class.
      *
      * <b>The variable <code>newPos</code> and <code>newNormal</code> must be
      * defined in the main section of the indjected shader.</b>
      *
-     * @typedef
      *
-     * @param {Object} options                Options for material.
-     * @param {string} options.baseMaterial   Base Material. The material whos fragment shader is used. Any type from the exported <code>TYPES</code> object
-     * @param {Object} options.vShader        Custom Vertex Shader
-     * @param {string} options.vShader.defines        Constant definitions like - "#define PI = 3.14;"
-     * @param {string} options.vShader.header         Code to be injected above main. Place function definitions here.
-     * @param {string} options.vShader.main           Code to be injected above main. Put main shader code here.
-     * @param {Object} options.uniforms       Custom Uniforms to be passed to the shader.
-     * @param {Object} options.passthrough    Any custom options to be passed to the underlying base material.
+     * @param {Object} options                    Options for material.
+     * @param {string} options.baseMaterial       Base Material. The material whos fragment shader is used. Any type from the exported <code>TYPES</code> object
+     * @param {...CustomShader} options.vShader   Custom Vertex Shader
+     * @param {...CustomShader} options.fShader   Custom Fragment Shader
+     * @param {Object} options.uniforms           Custom Uniforms to be passed to the shader.
+     * @param {Object} options.passthrough        Any custom options to be passed to the underlying base material.
      */
     constructor(options) {
-      const { baseMaterial, vShader } = options;
-      const uniforms = options.uniforms ?? [];
-      const passthrough = options.passthrough ?? {};
-      const baseShader = THREE__namespace.ShaderLib[baseMaterial];
+      if (options.vShader === undefined)
+        throw new Error("Vertex Shader must be provided.");
 
-      if (baseShader === undefined) throw "Invalid Base Material Type";
+      if (options.baseMaterial === undefined && options.fShader === undefined)
+        throw new Error(
+          "Fragment Shader must be provided when base mateiral is absent."
+        );
+
+      const baseMaterial = options.baseMaterial || TYPES.BASIC;
+      const vShader = options.vShader || {};
+      const fShader = options.fShader || {};
+      const uniforms = options.uniforms || [];
+      const passthrough = options.passthrough || {};
+      const baseShader = THREE__namespace.ShaderLib[baseMaterial];
 
       super({
         uniforms: THREE__namespace.UniformsUtils.merge([baseShader.uniforms, ...uniforms]),
@@ -70,8 +83,8 @@ var THREE_CustomShaderMaterial = (function (exports, THREE) {
           derivatives: true,
         },
 
-        vertexShader: _patchShader(THREE__namespace.ShaderChunk.meshphysical_vert, vShader),
-        fragmentShader: baseShader.fragmentShader,
+        vertexShader: _patchvShader(THREE__namespace.ShaderChunk.meshphysical_vert, vShader),
+        fragmentShader: _patchfShader(baseShader.fragmentShader, fShader),
         lights:
           baseMaterial === TYPES.PHYSICAL ||
           baseMaterial === TYPES.TOON ||
@@ -94,10 +107,27 @@ var THREE_CustomShaderMaterial = (function (exports, THREE) {
     }
   }
 
+  function _patchfShader(shader, { defines = "", header = "", main = "" }) {
+    const patchedShader = shader.replace(
+      "void main() {",
+      `
+    ${header}
+    void main() {
+      ${main}
+      ${main === "" ? "" : "return;"}
+    `
+    );
+
+    return `
+    ${defines}
+    ${patchedShader}
+    `;
+  }
+
   /**
    * From https://codepen.io/marco_fugaro/pen/xxZWPWJ?editors=0010
    */
-  function _patchShader(shader, { defines = "", header = "", main = "" }) {
+  function _patchvShader(shader, { defines = "", header = "", main = "" }) {
     let patchedShader = shader;
 
     const replaces = {
