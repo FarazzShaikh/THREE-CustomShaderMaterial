4d4c51fe71913be8bab9a1e074a7d5f17bf1f027
Faraz Shaikh <farazzshaikh@gmail.com>
2021-05-17 13:47:33 +0530

    Rebuilt demo site to accommodate more examples

--- example/Grapher/main.js
+++ example/Grapher/main.js
@@ -2,21 +2,21 @@ import * as THREE from "three";
 
 import { loadShadersCSM } from "../lib/glNoise/build/glNoise.m.js";
 
-import{camera} from "./components.js";
+import { camera } from "./components.js";
 
-import{scene,buildScene,createEnvMap} from './scene.js';
+import { scene, buildScene, createEnvMap } from "./scene.js";
 
-import{buildMaterial} from "./material.js";
-
-import{shaderVars,createFragmentScene,updateFragmentUniforms} from "./fragScene.js";
-
-import{renderer,controls} from './components.js';
+import { buildMaterial } from "./material.js";
 
 import {
-    ui,
-    createUI
-} from './ui.js';
+  shaderVars,
+  createFragmentScene,
+  updateFragmentUniforms,
+} from "./fragScene.js";
 
+import { renderer, controls } from "./components.js";
+
+import { ui, createUI } from "./ui.js";
 
 const paths = {
   defines: "./vertex/defines.glsl",
@@ -24,82 +24,59 @@ const paths = {
   main: "./vertex/main.glsl",
 };
 
+let shaderTex = new THREE.WebGLRenderTarget(1024, 1024);
 
-let shaderTex=new THREE.WebGLRenderTarget(1024,1024);
+const updateVertexUniforms = function (material, time) {
+  // material.transparent=true;
+  material.transmission = 1 - ui.opacity;
 
+  material.uniforms.uTime.value = time;
+  material.uniforms.rotX.value = ui.rotx;
+  material.uniforms.rotY.value = ui.roty;
+  material.uniforms.rotU.value = ui.rotu;
+  material.uniforms.tumble.value = ui.tumble;
+  material.uniforms.proj.value = ui.proj;
+  material.uniforms.baseRad.value = ui.baseRad;
+  material.uniforms.fn.value = ui.power;
+};
 
+let time = 0;
 
+const animate = function () {
+  requestAnimationFrame(animate);
+  controls.update();
 
+  //renderer.render(scene, camera);
 
-const updateVertexUniforms=function(material,time){
-    // material.transparent=true;
-    material.transmission=1.-ui.opacity;
+  // //render the texture for the main surface
+  renderer.setRenderTarget(shaderTex);
+  renderer.render(shaderVars.scene, shaderVars.camera);
+  //
+  // //render the main scene
+  renderer.setRenderTarget(null);
+  renderer.render(scene, camera);
 
-    material.uniforms.uTime.value =time;
-    material.uniforms.rotX.value=ui.rotx;
-    material.uniforms.rotY.value=ui.roty;
-    material.uniforms.rotU.value=ui.rotu;
-    material.uniforms.tumble.value=ui.tumble;
-    material.uniforms.proj.value=ui.proj;
-    material.uniforms.baseRad.value=ui.baseRad;
-    material.uniforms.fn.value=ui.power;
-  }
+  updateFragmentUniforms(time);
 
+  updateVertexUniforms(mat, time);
 
-
-let time=0.;
-
-  const animate = function () {
-    requestAnimationFrame(animate);
-    controls.update();
-
-
-    //renderer.render(scene, camera);
-
-
-      // //render the texture for the main surface
-       renderer.setRenderTarget(shaderTex);
-       renderer.render(shaderVars.scene,shaderVars.camera);
-      //
-      // //render the main scene
-       renderer.setRenderTarget(null);
-       renderer.render(scene, camera);
-
-       updateFragmentUniforms(time);
-
-       updateVertexUniforms(mat,time);
-
-         time+=0.01;
-        //const canvas = renderer.domElement;
-      //mat.uniforms.uResolution.value.set(canvas.width, canvas.height, 1);
-
-  };
-
-
-
-
-
-
-
+  time += 0.01;
+  //const canvas = renderer.domElement;
+  //mat.uniforms.uResolution.value.set(canvas.width, canvas.height, 1);
+};
 
 //running things:
 let mat;
- loadShadersCSM(paths).then((vertex) => {
+loadShadersCSM(paths).then((vertex) => {
+  createUI();
 
-     createUI();
+  let env = createEnvMap();
 
-    let env=createEnvMap();
+  createFragmentScene();
 
-    createFragmentScene();
+  mat = buildMaterial(vertex, env, shaderTex.texture);
 
-    mat=buildMaterial(vertex,env,shaderTex.texture);
+  buildScene(mat);
 
-    buildScene(mat);
-
-    animate();
-
+  animate();
 });
-
-
-
-
--- example/Grapher/ui.js
+++ example/Grapher/ui.js
@@ -2,10 +2,7 @@
 //Imports from lib/
 //=============================================
 
-
-import {
-    GUI
-} from './lib/dat.gui.module.js';
+import { GUI } from "../lib/dat.gui.module.js";
 
 //=============================================
 //Imports from My Code
@@ -13,77 +10,60 @@ import {
 
 //NONE HERE
 
-
-
 //=============================================
 //Variables Defined in this File
 //=============================================
 
-
-
-
 let ui = {
+  rotx: 0,
+  roty: 1,
+  rotu: 0,
+  tumble: 0.2,
+  fn: 1,
+  grid: 0.5,
+  hue: 1,
+  power: -1,
 
-    rotx:0,
-    roty:1,
-    rotu:0,
-    tumble: 0.2,
-    fn:1,
-    grid:0.5,
-    hue:1,
-    power:-1,
-
-    baseRad:10.,
-    proj:0,
-    opacity:0.85,
-    
+  baseRad: 10,
+  proj: 0,
+  opacity: 0.85,
 };
-
-
-
-
-
 
 //=============================================
 //Functions to Export
 //=============================================
 
-
 function createUI() {
-    let mainMenu = new GUI();
+  let mainMenu = new GUI();
 
-    mainMenu.width = 300;
+  mainMenu.width = 300;
 
-    mainMenu.domElement.style.userSelect = 'none';
+  mainMenu.domElement.style.userSelect = "none";
 
-    mainMenu.add(ui,'power',-5,5,1).name('n: f(z)=z^n');
+  mainMenu.add(ui, "power", -5, 5, 1).name("n: f(z)=z^n");
 
-    let more = mainMenu.addFolder('More');
-        more.add(ui,'grid',0,1,0.01).name('grid');
-        more.add(ui,'hue',0,1,0.01).name('hue');
-        more.add(ui,'opacity',0,1,0.01);
-        more.add(ui,'baseRad',0,10,0.1).name('diskRadis');
-        more.add(ui,'tumble',0,1,0.01).name('tumble');
-        more.add(ui,'rotx',0,1,0.01).name('rotx');
-        more.add(ui,'roty',0,1,0.01).name('roty');
-        more.add(ui,'rotu',0,1,0.01).name('rotu');
+  let more = mainMenu.addFolder("More");
+  more.add(ui, "grid", 0, 1, 0.01).name("grid");
+  more.add(ui, "hue", 0, 1, 0.01).name("hue");
+  more.add(ui, "opacity", 0, 1, 0.01);
+  more.add(ui, "baseRad", 0, 10, 0.1).name("diskRadis");
+  more.add(ui, "tumble", 0, 1, 0.01).name("tumble");
+  more.add(ui, "rotx", 0, 1, 0.01).name("rotx");
+  more.add(ui, "roty", 0, 1, 0.01).name("roty");
+  more.add(ui, "rotu", 0, 1, 0.01).name("rotu");
 
-        more.add(ui, 'proj', {
-        'Orthographic': 0,
-        'Perspective': 1
-    }).name('Projection');
+  more
+    .add(ui, "proj", {
+      Orthographic: 0,
+      Perspective: 1,
+    })
+    .name("Projection");
 
-    more.close();
+  more.close();
 }
-
-
 
 //=============================================
 //Exports from this file
 //=============================================
 
-
-export {
-    ui,
-    createUI
-};
+export { ui, createUI };
