2fd8f3cfbc9561e2680fd9da8a3a99cee29d48ff
Hendrik Mans <hendrik@mans.de>
2022-08-15 17:18:47 +0200

    Fix type in link script

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,4 +1,4 @@
-rm -rf ./node_modeules ./yarn.lock
+rm -rf ./node_modules ./yarn.lock
 yarn
 yarn build
 
@@ -18,7 +18,7 @@ cd ../../../
 
 for d in ./examples/* ; do
     cd $d
-    rm -rf ./node_modeules ./yarn.lock
+    rm -rf ./node_modules ./yarn.lock
     yarn
 
     yarn link three-custom-shader-material
