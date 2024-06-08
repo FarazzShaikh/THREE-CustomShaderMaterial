aa15919e9f664cd893899a2daf5fc56d110cb9a1
Faraz Shaikh <farazzshaikh@gmail.com>
2024-06-08 16:49:19 +0400

    Examples: Add base URl to react router

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,26 +5,31 @@ import { SHADERS } from "./Examples";
 import { NotFound } from "./pages/NotFound";
 import { Root } from "./pages/Root";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        ...Object.values(SHADERS).map((preset) => ({
+          path: preset.slug,
+          element: <preset.Component />,
+        })),
+        {
+          path: "/",
+          element: <SHADERS.DEFAULT.Component />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      ...Object.values(SHADERS).map((preset) => ({
-        path: preset.slug,
-        element: <preset.Component />,
-      })),
-      {
-        path: "/",
-        element: <SHADERS.DEFAULT.Component />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
