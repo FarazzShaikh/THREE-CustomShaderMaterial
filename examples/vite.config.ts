47c4c9c3a2cdb1eb44e6dd4a43eb33ecce922b28
Faraz Shaikh <farazzshaikh@gmail.com>
2024-09-22 01:35:36 +0400

    Add vite base

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/THREE-CustomShaderMaterial/",
   plugins: [react()],
   resolve: {
     alias: {
