b335bfae82aa4b77d87e1f6da08e9380a9dbfdb0
Faraz Shaikh <farazzshaikh@gmail.com>
2024-09-22 00:01:36 +0400

    Update actions

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/THREE-CustomShaderMaterial/",
   plugins: [react()],
   resolve: {
     alias: {
