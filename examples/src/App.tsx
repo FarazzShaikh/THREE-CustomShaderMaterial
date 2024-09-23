1c10d74c6db49f834c3f7de6a4b916d8253840f7
Faraz Shaikh <farazzshaikh@gmail.com>
2024-09-23 23:25:24 +0400

    Swap to hash router

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,35 +1,30 @@
 import "./styles/index.css";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import { SHADERS } from "./Examples";
 import { NotFound } from "./pages/NotFound";
 import { Root } from "./pages/Root";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        ...Object.values(SHADERS).map((preset) => ({
-          path: preset.slug,
-          element: <preset.Component />,
-        })),
-        {
-          path: "/",
-          element: <SHADERS.DEFAULT.Component />,
-        },
-      ],
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,
-  }
-);
+    path: "/",
+    element: <Root />,
+    children: [
+      ...Object.values(SHADERS).map((preset) => ({
+        path: preset.slug,
+        element: <preset.Component />,
+      })),
+      {
+        path: "/",
+        element: <SHADERS.DEFAULT.Component />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
